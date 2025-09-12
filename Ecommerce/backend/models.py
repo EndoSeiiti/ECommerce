a42fcd8dc823a92af5c7394ba177f4f02a3b126e
EndoSeiiti <endoseiiti@gmail.com>
2025-09-11 21:11:41 -0300

    feat:add models for e-commerce

---
+++ Ecommerce/backend/models.py
@@ -0,0 +1,37 @@
+from . import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+    
+
+    
+
+class Post(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False )
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"),nullable=False)
+    price = db.Column(db.Float,nullable=False)
+    rating = db.Column(db.Float)
+    purchases = db.Column(db.Integer)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+class Order(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id',ondelete="CASCADE",nullable=False))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    status= db.Column(db.Text)
+
+
+class Comment(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE", nullable=False))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    text=db.Column(db.Text,nullable=False)
+
+
